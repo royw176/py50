cda689305b7136136a2a9c941237a96f861ec71f
royw176 <crazy99men@gmail.com>
2022-10-12 17:04:54 +0800

    Q1 answer

---
+++ day04/Quetion1.py
@@ -0,0 +1,14 @@
+'''
+Description: Day4-symbol of operation
+Version: 0.1
+Author: Roy
+'''
+# Q1: Degrees Fahrenheit convert to Celsius.
+# Hint function: C = (F - 32) / 1.8
+f = float(input("Please enter the degrees Fahrenheit: "))
+c = float((f-32)/1.8)
+print("Degrees Celsius is : %.2f" % c)
+
+# Reference:
+# https://www.techiedelight.com/zh-tw/restrict-floats-to-two-decimal-points-python/
+
