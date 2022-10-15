c0c16e33a5405f3480d6db127b03c2150d952b72
royw176 <crazy99men@gmail.com>
2022-10-15 11:08:26 +0800

    loop structure

--- day06/quetion1.py
+++ day06/quetion1.py
@@ -6,15 +6,8 @@ Author: Roy
 
 # Q1: 英制单位英寸与公制单位厘米互换。
 # Hint function: 1 inch = 2.54 cm
-
-unit = input("Enter the unit (inch or cm): ")
-value = float(input("Enter the value: "))
-
-if unit == "inch":
-    print("%.2f inch is %.2f cm : " %(value, value*2.54))
-elif unit == "cm":
-    print("%.2f cm is %.2f inch : " %(value, value/2.54))
-else:
-    print("Please enter inch or cm!")
-
-# Extend issue: How to check the value is digital?
+ 
+ total=0
+ for x in range(1, 101):
+    total += x
+print(x)
