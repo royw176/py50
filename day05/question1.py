dc86c7c89efd41d697a93f8be2a28ffcec061d71
royw176 <crazy99men@gmail.com>
2022-10-17 11:27:57 +0800

    Q1 answer

---
+++ day05/question1.py
@@ -0,0 +1,20 @@
+'''
+Description: Day5 if...else statement
+Version: 0.1
+Author: Roy
+'''
+
+# Q1: 英制单位英寸与公制单位厘米互换。
+# Hint function: 1 inch = 2.54 cm
+
+unit = input("Enter the unit (inch or cm): ")
+value = float(input("Enter the value: "))
+
+if unit == "inch":
+    print("%.2f inch is %.2f cm : " %(value, value*2.54))
+elif unit == "cm":
+    print("%.2f cm is %.2f inch : " %(value, value/2.54))
+else:
+    print("Please enter inch or cm!")
+
+# Extend issue: How to check the value is digital?
